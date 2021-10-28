415e09199a5a176bdcd15146dca41d1a11e279e5
tonykhanhvo <vgkhanh1997@gmail.com>
2021-10-28 16:27:51 +0700

    excercise 01 success

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Card, CardImg, CardImgOverlay, CardBody, CardTitle, CardText } from 'reactstrap';
+import DishDetail from './DishdetailComponent';
 
 class Menu extends Component {
 
@@ -15,23 +16,23 @@ class Menu extends Component {
     this.setState({ selectedDish: dish});
   }
 
-  renderDish(dish) {
-    if (dish != null) {
-      return (
-        <Card>
-          <CardImg width="100%" src={dish.image} alt={dish.name} />
-          <CardBody>
-            <CardTitle>{dish.name}</CardTitle>
-            <CardText>{dish.description}</CardText>
-          </CardBody>
-        </Card>
-      );
-    } else {
-      return (
-        <div></div>
-      );
-    }
-  }
+  // renderDish(dish) {
+  //   if (dish != null) {
+  //     return (
+  //       <Card>
+  //         <CardImg width="100%" src={dish.image} alt={dish.name} />
+  //         <CardBody>
+  //           <CardTitle>{dish.name}</CardTitle>
+  //           <CardText>{dish.description}</CardText>
+  //         </CardBody>
+  //       </Card>
+  //     );
+  //   } else {
+  //     return (
+  //       <div></div>
+  //     );
+  //   }
+  // }
 
   render() {
 
@@ -53,9 +54,7 @@ class Menu extends Component {
         <div className="row">
           {menu}
         </div>
-        <div className="row">
-          {this.renderDish(this.state.selectedDish)}
-        </div>
+        <DishDetail dish={this.state.selectedDish} />
       </div>
     );
   }
