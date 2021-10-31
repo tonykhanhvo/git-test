7540a6e60984b39f319159070776fd049e9e8a04
tonykhanhvo <vgkhanh1997@gmail.com>
2021-10-31 10:55:29 +0700

    modify StaffList Component: remove ifstatement in renderStaff and create StaffDetail component

---
+++ src/components/StaffDetailComponent.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { Card, CardImg, CardImgOverlay, CardBody, CardTitle, CardText } from 'reactstrap';
+
+
+class StaffDetail extends Component {
+  constructor(props) {
+    super(props)
+
+  }
+
+  render() {
+    
+  }
+}
--- src/components/StaffListComponent.js
+++ src/components/StaffListComponent.js
@@ -7,32 +7,30 @@ class StaffList extends Component {
   constructor(props) {
     super(props)
     
+    this.state = {
+      selectedStaff : null
+    }
   }
 
   renderStaff() {
     const staffs = this.props.staffs;
-
-    if (staffs != null) {
-      const staffList = staffs.map((staff) => {
-        return (
-          <div key={staff.id} className="col-12 col-md-6 col-lg-4 m-1">
-            <Card>
-              <CardImg width="100%" src={staff.image} alt={staff.name} />
-              <CardImgOverlay>
-                <CardTitle>{staff.name}</CardTitle>
-              </CardImgOverlay>
-            </Card>
-          </div>
-        );
-      });
-      return (staffList);
-    } else {
-      return (<div></div>);
-    }
+  
+    const staffList = staffs.map((staff) => {
+      return (
+        <div key={staff.id} className="col-12 col-md-6 col-lg-4 m-1">
+          <Card>
+            <CardImg width="100%" src={staff.image} alt={staff.name} />
+            <CardImgOverlay>
+              <CardTitle>{staff.name}</CardTitle>
+            </CardImgOverlay>
+          </Card>
+        </div>
+      );
+    })
+    return (staffList);
   }
 
   render() {
-
     return (
       <div className="container">
             <div className="row">
