33b54ff4de558c9a708d548b6b36cdc17849b272
tonykhanhvo <vgkhanh1997@gmail.com>
2021-10-31 10:46:35 +0700

    create renderStaff method to render staff list

--- src/components/StaffListComponent.js
+++ src/components/StaffListComponent.js
@@ -6,12 +6,40 @@ class StaffList extends Component {
 
   constructor(props) {
     super(props)
+    
+  }
 
+  renderStaff() {
+    const staffs = this.props.staffs;
 
+    if (staffs != null) {
+      const staffList = staffs.map((staff) => {
+        return (
+          <div key={staff.id} className="col-12 col-md-6 col-lg-4 m-1">
+            <Card>
+              <CardImg width="100%" src={staff.image} alt={staff.name} />
+              <CardImgOverlay>
+                <CardTitle>{staff.name}</CardTitle>
+              </CardImgOverlay>
+            </Card>
+          </div>
+        );
+      });
+      return (staffList);
+    } else {
+      return (<div></div>);
+    }
   }
 
   render() {
 
+    return (
+      <div className="container">
+            <div className="row">
+              {this.renderStaff}
+            </div>
+          </div>
+    );
   }
 }
 
