2ac68304b158f4bee99d1e1504eb1ccc31360580
tonykhanhvo <vgkhanh1997@gmail.com>
2021-11-02 19:54:51 +0700

    create Container Component be called Main Component, MenuComponent and Dishdetial Component be called Presentational Component

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -32,19 +32,21 @@ class DishDetail extends Component {
     
     if (dish != null) {
       return (
-        <div className="row">
-          <div className="col-12 col-md-5 m-1">
-            <Card>
-              <CardImg width="100%" src={dish.image} alt={dish.name} />
-              <CardBody>
-                <CardTitle>{dish.name}</CardTitle>
-                <CardText>{dish.description}</CardText>
-              </CardBody>
-            </Card>
-          </div>
-          <div className="col-12 col-md-5 m-1">
-            <h4>Comments</h4>
-            {this.renderComments(dish)}
+        <div className="container">
+          <div className="row">
+            <div className="col-12 col-md-5 m-1">
+              <Card>
+                <CardImg width="100%" src={dish.image} alt={dish.name} />
+                <CardBody>
+                  <CardTitle>{dish.name}</CardTitle>
+                  <CardText>{dish.description}</CardText>
+                </CardBody>
+              </Card>
+            </div>
+            <div className="col-12 col-md-5 m-1">
+              <h4>Comments</h4>
+              {this.renderComments(dish)}
+            </div>
           </div>
         </div>
       );
