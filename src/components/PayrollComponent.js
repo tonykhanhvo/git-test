9123c77f325cfbd19b7fca935ff57190e81631c5
tonykhanhvo <vgkhanh1997@gmail.com>
2021-11-17 12:17:48 +0700

    Update css Payroll Component

--- src/components/PayrollComponent.js
+++ src/components/PayrollComponent.js
@@ -8,9 +8,9 @@ function RenderPayrollList({ staff, stt }) {
 
   return (
     <tr key={staff.id} > 
-      <th scope="row">
+      <td scope="row">
         {stt + 1}
-      </th>
+      </td>
       <td>
         {staff.name}
       </td>
@@ -23,9 +23,9 @@ function RenderPayrollList({ staff, stt }) {
       <td>
       {staff.overTime}
       </td>
-      <td>
-        {parseInt((basicSalary*staff.salaryScale + overTimeSalary*staff.overTime),10)}
-      </td>
+      <th>
+        {(parseInt((basicSalary*staff.salaryScale + overTimeSalary*staff.overTime),10)).toLocaleString("vi-VN")}
+      </th>
     </tr>
   );
 }
@@ -40,33 +40,43 @@ const PayrollList = ({ staffs }) => {
 
   return (
     <div className="container">
-      <Table striped>
-        <thead>
-          <tr>
-            <th>
-              STT
-            </th>
-            <th>
-              Họ và tên
-            </th>
-            <th>
-              Phòng Ban
-            </th>
-            <th>
-              Hệ số lương
-            </th>
-            <th>
-              Giờ làm thêm
-            </th>
-            <th>
-              Lương
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {payrolllist}
-        </tbody>
-      </Table>
+      <div className="row">
+        <div className="col-12">
+          <h3>Bảng Lương</h3>
+          <hr />
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-12">
+          <Table striped className="table-text">
+            <thead>
+              <tr>
+                <th>
+                  STT
+                </th>
+                <th>
+                  Họ và tên
+                </th>
+                <th>
+                  Phòng Ban
+                </th>
+                <th>
+                  Hệ số lương
+                </th>
+                <th>
+                  Giờ làm thêm
+                </th>
+                <th>
+                  Lương
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {payrolllist}
+            </tbody>
+          </Table>
+        </div>
+      </div>
     </div>
   );
 }
