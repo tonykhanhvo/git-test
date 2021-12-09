b439180067072b132c75ef6da72ba99df8e7d139
tonykhanhvo <vgkhanh1997@gmail.com>
2021-12-09 22:42:48 +0700

    Create ActionType, ActionCreators, reducers: staffs, departments; initial forms, update configureStore

---
+++ src/redux/actions/ActionTypes.js
@@ -0,0 +1,10 @@
+//Add Staff Actions
+export const ADD_STAFFS = 'ADD_STAFFS';
+export const STAFFS_LOADING = 'STAFFS_LOADING';
+export const STAFFS_FAILED = 'STAFFS_FAILED';
+
+//Add Department Actions
+export const ADD_DEPARTMENTS = 'ADD_DEPARTMENTS';
+export const DEPARTMENTS_LOADING = 'DEPARTMENTS_LOADING';
+export const DEPARTMENTS_FAILED = 'DEPARTMENTS_FAILED';
+
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,10 +1,21 @@
-import { createStore } from "redux";
-import { Reducer, initialState } from './reducers/reducer';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createForms } from 'react-redux-form';
+import { Staffs } from './reducers/staffs';
+import { Departments } from './reducers/departments';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import { InitialForm } from './forms';
 
 export const ConfigureStore = () => {
   const store = createStore(
-    Reducer,
-    initialState
+    combineReducers({
+      staffs: Staffs,
+      departments: Departments,
+      ...createForms({
+        form: InitialForm
+      })
+    }),
+    applyMiddleware(thunk, logger)
   );
 
   return store;
